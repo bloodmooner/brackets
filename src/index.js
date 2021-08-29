b33dca5d182b5d783d5134ac36cbfdf88aadb40c
Sergey Goncharuk <fleshed914@gmail.com>
2021-08-29 13:59:53 +0300

    feat: add brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,96 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const OPEN_BRACKETS = [];
+    const BRACKETS_PAIR = {}
+
+    console.log(BRACKETS_PAIR);
+
+    for(let i = 0; i < bracketsConfig.length; i++) {
+        let bracketsOpen = bracketsConfig[i][0];
+        let bracketsClose = bracketsConfig[i][1];
+
+        OPEN_BRACKETS.push(bracketsOpen);
+        BRACKETS_PAIR[[bracketsClose]] = bracketsOpen;
+    }
+
+    console.log(OPEN_BRACKETS);
+    console.log(BRACKETS_PAIR);
+    
+
+    let stack = [];
+
+    for(let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+
+        if(OPEN_BRACKETS.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+        } else {
+            if(stack.length === 0) {
+                return false;
+            }
+
+            let topElement = stack[stack.length - 1];
+
+            if(BRACKETS_PAIR[currentSymbol] === topElement) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
+
+
+
+/* const check = (str, bracketsConfig) => {
+    const OPEN_BRACKETS = [];
+    const BRACKETS_PAIR = {}
+
+    console.log(BRACKETS_PAIR);
+
+    for(let i = 0; i < bracketsConfig.length; i++) {
+        let bracketsOpen = bracketsConfig[i][0];
+        let bracketsClose = bracketsConfig[i][1];
+
+        OPEN_BRACKETS.push(bracketsOpen);
+        BRACKETS_PAIR[[bracketsClose]] = bracketsOpen;
+    }
+
+    console.log(OPEN_BRACKETS);
+    console.log(BRACKETS_PAIR);
+    
+
+    let stack = [];
+
+    for(let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+
+        if(OPEN_BRACKETS.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+        } else {
+            if(stack.length === 0) {
+                return false;
+            }
+
+            let topElement = stack[stack.length - 1];
+
+            if(BRACKETS_PAIR[currentSymbol] === topElement) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+
+console.log(check(
+    '[]()', 
+    [
+        ['(', ')'], ['[', ']']
+    ]
+));
+ */
